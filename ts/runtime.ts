7be4f232cea2904910c639a99573a4ff20e88b97
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-12-18 20:27:17 +0100

    Add Process to AppRuntime

--- ts/runtime.ts
+++ ts/runtime.ts
@@ -2,11 +2,12 @@ import type { App, AppMutator } from "$types/app";
 import { AppRuntime } from "$ts/apps";
 import { StateHandler } from "$ts/states";
 import { SettingsStore } from "./store";
+import { Process } from "$ts/process";
 
 export class Runtime extends AppRuntime {
   state: StateHandler;
-  constructor(app: App, mutator: AppMutator) {
-    super(app, mutator);
+  constructor(app: App, mutator: AppMutator, process: Process) {
+    super(app, mutator, process);
 
     this.state = new StateHandler(app.id, SettingsStore, "account")
   }
