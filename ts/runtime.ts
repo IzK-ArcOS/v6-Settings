989f8bcd854f81f321e8eb8b7886f19bb273b29a
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-12-18 17:38:23 +0100

    Add `AppMutator`

--- ts/runtime.ts
+++ ts/runtime.ts
@@ -1,12 +1,12 @@
-import type { App } from "$types/app";
+import type { App, AppMutator } from "$types/app";
 import { AppRuntime } from "$ts/apps";
 import { StateHandler } from "$ts/states";
 import { SettingsStore } from "./store";
 
 export class Runtime extends AppRuntime {
   state: StateHandler;
-  constructor(app: App) {
-    super(app);
+  constructor(app: App, mutator: AppMutator) {
+    super(app, mutator);
 
     this.state = new StateHandler(app.id, SettingsStore, "account")
   }
