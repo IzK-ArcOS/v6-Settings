97692155f947281a240e3c142681291b121845a9
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-02-19 11:37:39 +0100

    Add license to about page

--- Components/Page/About.svelte
+++ Components/Page/About.svelte
@@ -3,24 +3,31 @@
   import { ArcOSVersion } from "$ts/env";
   import { GetHelp } from "$ts/help";
   import { ARCOS_BUILD, ARCOS_MODE } from "$ts/metadata";
+  import { getPartialFile, readFile } from "$ts/server/fs/file";
+  import { openFileWithApp } from "$ts/server/fs/open";
   import { HelpArticles } from "$ts/stores/articles";
 
   function contact() {
     GetHelp(HelpArticles.contact);
+  }
+
+  async function license() {
+    const partial = await getPartialFile("@client/README.md");
+    openFileWithApp("MarkDownViewer", partial);
   }
 </script>
 
 <GlowingLogo className="logo" />
 <h1>ArcOS <span class="colored">v6</span></h1>
 <p>
-  Thank you for using ArcOS! We're constantly working on improving it to give
-  you the best experience possible. Curious who works on ArcOS? Go ahead and
-  visit our Frontend GitHub page, we list our contributors there.
+  Thank you for using ArcOS! We're constantly working on improving it to give you the best
+  experience possible. Curious who works on ArcOS? Go ahead and visit our Frontend GitHub page, we
+  list our contributors there.
   <br /><br />
   Want to contact us?
   <button class="link" on:click={contact}>Do so here</button>.
 </p>
 <p class="version">
   v{ArcOSVersion}-{ARCOS_MODE}
-  {ARCOS_BUILD}
+  {ARCOS_BUILD} - <button class="link" on:click={license}>License</button>
 </p>
